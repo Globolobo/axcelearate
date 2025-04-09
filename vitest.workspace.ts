e87a38162aefa99f79d1f540ca372a36f05a0b1d
Globolobo <sean.pichardo@gmail.com>
2025-04-09 19:25:55 +1000

    wip: test suite

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,32 +1,48 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-import { defineWorkspace } from 'vitest/config';
+import { defineWorkspace, configDefaults } from "vitest/config";
 
-import { storybookTest } from '@storybook/experimental-addon-test/vitest-plugin';
+import { storybookTest } from "@storybook/experimental-addon-test/vitest-plugin";
+import react from "@vitejs/plugin-react";
 
 const dirname =
-  typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+  typeof __dirname !== "undefined"
+    ? __dirname
+    : path.dirname(fileURLToPath(import.meta.url));
 
 // More info at: https://storybook.js.org/docs/writing-tests/test-addon
 export default defineWorkspace([
-  'vite.config.ts',
+  "vite.config.ts",
   {
-    extends: 'vite.config.ts',
+    extends: "vite.config.ts",
     plugins: [
       // The plugin will run tests for the stories defined in your Storybook config
       // See options at: https://storybook.js.org/docs/writing-tests/test-addon#storybooktest
-      storybookTest({ configDir: path.join(dirname, '.storybook') }),
+      storybookTest({ configDir: path.join(dirname, ".storybook") }),
     ],
     test: {
-      name: 'storybook',
+      name: "storybook",
       browser: {
         enabled: true,
         headless: true,
-        name: 'chromium',
-        provider: 'playwright'
+        name: "chromium",
+        provider: "playwright",
       },
-      setupFiles: ['.storybook/vitest.setup.ts'],
+      setupFiles: [".storybook/vitest.setup.ts"],
     },
   },
+  // {
+  //   extends: "vite.config.ts",
+  //   plugins: [react()],
+  //   test: {
+  //     name: "dom",
+  //     browser: {
+  //       enabled: true,
+  //       provider: "playwright",
+  //       instances: [{ browser: "chromium" }],
+  //     },
+  //     exclude: [...configDefaults.exclude, "stories/*", "*/storybook.test.ts"],
+  //   },
+  // },
 ]);
